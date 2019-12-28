0de5247897b8ecf2a37b7a167f6f7756d9d26a9e
liuquan <liuquantj@yahoo.co.jp>
2019-12-28 11:42:51 +0800

    啊啊啊

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class FizzBuzz {
+    public static String of(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        }
+
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+
+        return String.valueOf(number);
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class FizzBuzzTest {
+    private int number;
+    private String expectedResult;
+
+    public FizzBuzzTest(int number, String expectedResult) {
+        this.number = number;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {1, "1"},
+                {3, "Fizz"},
+                {5, "Buzz"},
+                {15, "FizzBuzz"}
+        });
+    }
+
+    @Test
+    public void test() {
+        assertEquals(expectedResult, FizzBuzz.of(number));
+    }
 }
